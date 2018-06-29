252003fbb3386295b71b817664ccf7936c1a0c3a
wandesky <bwandesky@gmail.com>
2018-06-29 06:15:02 +0300

    set api versioning

---
+++ appv1/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+rides = [{'driverID' : '001', 'car-route':'routeA','rideID':'ride001'}, {'driverID' : '002', 'car-route':'routeB','rideID':'ride002'}, {'driverID' : '001', 'car-route':'routeA' ,'rideID':'ride003'}]
+
+@app.route('/', methods=['GET'])
+def test():
+    return jsonify({'message': "I should be displaying the homepage here"})
+
+@app.route('/rides', methods= ['GET'])
+def returnAll():
+    return jsonify({'rides': rides})
+
+@app.route('/rides/<string:rideID>', methods=['GET'])
+def returnOne(rideID):
+    results = [ride for ride in rides if ride['rideID'] == rideID]
+    return jsonify({'ride':results[0]})
+
+if __name__ == '__main__':
+    app.run()
