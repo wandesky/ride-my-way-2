1db04bed1671ad0cbaa92a633b0b673cf39adb92
wandesky <bwandesky@gmail.com>
2018-06-29 06:47:15 +0300

    ch update api versioning to include version 2

--- appv2/__init__.py
+++ appv2/__init__.py
@@ -4,18 +4,18 @@ app = Flask(__name__)
 
 rides = [{'driverID' : '001', 'car-route':'routeA','rideID':'ride001'}, {'driverID' : '002', 'car-route':'routeB','rideID':'ride002'}, {'driverID' : '001', 'car-route':'routeA' ,'rideID':'ride003'}]
 
-@app.route('/api/v1/users/', methods=['GET'])
+@app.route('/api/v2/users/', methods=['GET'])
 def test():
     return jsonify(
-        {'version': "You are now running version 1 of the api"},
+        {'version': "You are now running version 2 of the api"},
         {'instruction': "How to use the api"}
         )
 
-@app.route('/rides', methods= ['GET'])
+@app.route('/api/v2/users/rides', methods= ['GET'])
 def returnAll():
     return jsonify({'rides': rides})
 
-@app.route('/rides/<string:rideID>', methods=['GET'])
+@app.route('/api/v2/users/rides/<string:rideID>', methods=['GET'])
 def returnOne(rideID):
     results = [ride for ride in rides if ride['rideID'] == rideID]
     return jsonify({'ride':results[0]})
