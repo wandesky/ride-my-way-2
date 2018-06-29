fe80d93bdc1c8784d6ca485a029017609706ba99
wandesky <bwandesky@gmail.com>
2018-06-29 10:23:47 +0300

    set routes for v1

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 """App entry point"""
 
-"from appv1 import app"
+from appv1 import app
 
-from appv2 import app
+"from appv2 import app"
 
 
 if __name__ == '__main__':
--- appv1/__init__.py
+++ appv1/__init__.py
@@ -4,6 +4,15 @@ app = Flask(__name__)
 
 rides = [{'driverID' : '001', 'car-route':'routeA','rideID':'ride001'}, {'driverID' : '002', 'car-route':'routeB','rideID':'ride002'}, {'driverID' : '001', 'car-route':'routeA' ,'rideID':'ride003'}]
 
+@app.route('/', methods=['GET'])
+def test():
+    return jsonify(
+        {'version': "You are now running version 1 of the api"},
+        {'You should not be here': "Ideally, you are supposed to access the root page using <host>/api/v1/users/"},
+        {'So why are you seeing this': "This page was made just to let you know the way things are organised around here"}
+        )
+
+
 @app.route('/api/v1/users/', methods=['GET'])
 def test():
     return jsonify(
@@ -11,11 +20,11 @@ def test():
         {'instruction': "How to use the api"}
         )
 
-@app.route('/rides', methods= ['GET'])
+@app.route('/api/v1/users/rides', methods= ['GET'])
 def returnAll():
     return jsonify({'rides': rides})
 
-@app.route('/rides/<string:rideID>', methods=['GET'])
+@app.route('/api/v1/users/rides/<string:rideID>', methods=['GET'])
 def returnOne(rideID):
     results = [ride for ride in rides if ride['rideID'] == rideID]
     return jsonify({'ride':results[0]})
